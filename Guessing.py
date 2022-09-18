bd8fa2920ad80f97af19d79bd8399d467c5f6f73
Kirtan <kirtz_777@hotmail.co.uk>
2022-09-18 04:12:41 +0100

    FInished for now

--- Guessing.py
+++ Guessing.py
@@ -11,17 +11,29 @@ if top_of_range.isdigit():
         print("Please enter a number larger than 0")
         quit()
 else:
-    print("Please enter in a number.")
+    print("Please enter in a valid positive number.")
     quit()
 
 random_number = random.randint(0, top_of_range)
-
-print(random_number)
+guesses = 0 
 
 while True:
+    guesses += 1 #keep adding 1 each time the while loop is run
     user_guess = input("Guess the Number: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
 
-else:
-    print("Please enter in a number.")
+    else:
+        print("Please enter in a number.")
+        continue
+
+    if user_guess == random_number:
+        print("Well Done!")
+        break
+    
+    elif user_guess > random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+
+print("You got it in", guesses, "guesses") # Same as print("XXX " + str(guesses) + " YYY")
